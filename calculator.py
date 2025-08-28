5e3faede8832cba0c43c40543f8b73e7ef53b62e
tsehaynesh B. <tsehayneshbiruh2@gmail.com>
2025-08-28 12:36:13 +0300

    Create calculator.py
    calculater in phython in plp

---
+++ calculator.py
@@ -0,0 +1,19 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter an operation (+, -, *, /): ")
+
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error! Division by zero."
+else:
+    result = "Invalid operation!"
+
+print(f"{num1} {operation} {num2} = {result}")
